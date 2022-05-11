b76e150c799f531d88e038854daca24cb0ac2a22
Julian Mauricio Ferreira <jm.ferreira@uniandes.edu.co>
2022-05-10 21:29:59 -0500

    Filtros
    Filtro expedición, seguro  y proveedores.

--- src/app/expediciones/expediciones.module.ts
+++ src/app/expediciones/expediciones.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExpedicionesComponent } from './expediciones.component';
 import { ExpedicionCardModule } from '../components/expedicion-card/expedicion-card.module';
+import { FiltroModule } from '../components/filtro/filtro.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    ExpedicionCardModule
+    ExpedicionCardModule,
+    FiltroModule
   ],
   declarations: [ExpedicionesComponent],
   exports: [ExpedicionesComponent]
